b739a18fffac8a2584b9a23108e36a2c1cd2b484
Abhilasha Bhatia <abhilasha.bhatia17@gmail.com>
2023-12-03 00:17:49 -0500

    add endpoints to create and list a single poll

--- pollingapp/main.go
+++ pollingapp/main.go
@@ -24,6 +24,8 @@ func main() {
 
 	// Routes
 	r.GET("/polls", getPolls)
+	r.GET("/polls/:id", getPoll)
+	r.POST("/polls", createPoll)
 	r.PUT("/polls/:id/like", likePoll)
 	r.PUT("/polls/:id/dislike", dislikePoll)
 
@@ -46,6 +48,36 @@ func getPolls(c *gin.Context) {
 	var polls []Poll
 	db.Find(&polls)
 	c.JSON(200, polls)
+}
+
+func getPoll(c *gin.Context) {
+	var poll Poll
+	id := c.Param("id")
+
+	if err := db.First(&poll, id).Error; err != nil {
+		c.AbortWithStatus(404)
+		return
+	}
+
+	c.JSON(200, poll)
+}
+
+func createPoll(c *gin.Context) {
+	var input struct {
+		Question string `json:"question" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	poll := Poll{
+		Question: input.Question,
+	}
+
+	db.Create(&poll)
+	c.JSON(201, poll)
 }
 
 func likePoll(c *gin.Context) {
